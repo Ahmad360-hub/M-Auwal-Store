3bbc0edd1d7e3795a4192a2cb29b270c4f95a8af
Ahmad360-hub <muhammadauwalsani7@gmail.com>
2026-03-10 12:20:49 +0100

    Create app_colors.dart with color constants for the pharmacy app

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,21 @@
+// app_colors.dart
+
+class AppColors {
+  // Primary Colors
+  static const Color primaryColor = Color(0xFF6200EA);
+  static const Color primaryVariant = Color(0xFF3700B3);
+  static const Color secondaryColor = Color(0xFF03DAC6);
+
+  // Neutral Colors
+  static const Color neutralLight = Color(0xFFFFFFFF);
+  static const Color neutralDark = Color(0xFF000000);
+
+  // Status Colors
+  static const Color successColor = Color(0xFF4CAF50);
+  static const Color errorColor = Color(0xFFF44336);
+  static const Color warningColor = Color(0xFFFFC107);
+
+  // Background Colors
+  static const Color backgroundLight = Color(0xFFF5F5F5);
+  static const Color backgroundDark = Color(0xFF121212);
+}
